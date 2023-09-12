01b3db229645727581f18021da65541e920e20bd
MairaQuillQuest <ririsoniah@gmail.com>
2023-09-12 12:21:46 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,13 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * main - print Algorithm, followed by new line
- * Return: 0
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	_putchar('A');
-	_putchar('l');
-	_putchar('g');
-	_putchar('o');
-	_putchar('r');
-	_putchar('i');
-	_putchar('t');
-	_putchar('h');
-	_putchar('m');
-	_putchar('\n');
-
+	_putchar("_putchar\n");
 	return (0);
 }

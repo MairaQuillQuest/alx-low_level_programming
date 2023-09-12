ce1471f02235461adb572923a924c19ddeb23aeb
MairaQuillQuest <ririsoniah@gmail.com>
2023-09-12 11:05:08 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "algorithm.h"
+
+/**
+ * main - print Algorithm, followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('A');
+	_putchar('l');
+	_putchar('g');
+	_putchar('o');
+	_putchar('r');
+	_putchar('i');
+	_putchar('t');
+	_putchar('h');
+	_putchar('m');
+	_putchar('\n');
+
+	return (0);
+}

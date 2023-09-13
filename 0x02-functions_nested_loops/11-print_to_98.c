8d0085860d843d1fe270d2c234ca656b89de638c
MairaQuillQuest <ririsoniah@gmail.com>
2023-09-12 17:31:07 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,50 +1,31 @@
 #include "main.h"
+#include "6-abs.c"
+#include <stdio.h>
 
-/**
- * print_times_table - print multiplication table up to n
- * @n: integer argument
- */
-
-void print_times_table(int n)
+void print_to_98(int n)
 {
-	int row;
-	int column;
-	int product;
+	int i;
 
-	if (n >= 0 && n <= 15)
+	if (n > 98)
 	{
-		for (row = 0; row <= n; row++)
+		for (i = n; i >= 98; i--)
 		{
-			for (column = 0; column <= n; column++)
+			printf("%d", i);
+			if (i != 98)
 			{
-				product = (row * column);
-				if (column == 0)
-					_putchar('0' + product);
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					if (product <= 9)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + product);
-					}
-					else if (product > 9 && product < 100)
-					{
-						_putchar(' ');
-						_putchar('0' + (product / 10));
-						_putchar('0' + (product % 10));
-					}
-					else if (product >= 100)
-					{
-						_putchar('0' + (product / 100));
-						_putchar('0' + ((product / 10) % 10));
-						_putchar('0' + (product % 10));
-					}
-				}
+				printf(", ");
 			}
-			_putchar('\n');
+		}
+	} else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
 	}
+	printf("\n");
 }

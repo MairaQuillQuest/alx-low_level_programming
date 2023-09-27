0bf64128098a8dcb859e64b8cae7245e047abdf4
MairaQuillQuest <ririsoniah@gmail.com>
2023-09-27 13:19:09 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,22 +1,58 @@
 #include "main.h"
 
 /**
- * _pow_recursion - raise x to power y
- * @x: number
- * @y: power to raise to
- * Return: power of a number, -1 if power less than 0
+ * wildcmp - compare two strings with "wildcard expansion" capabilities
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if strings can be considered identical, else 0
  */
 
-int _pow_recursion(int x, int y)
+int wildcmp(char *s1, char *s2)
 {
-	if (y < 0)
-		return (-1);
-
-	if (x == 0)
-		return (0);
-
-	if (x == 1 || y == 0)
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
+	else if (*s1 == '\0' || *s2 == '\0')
+	{
+		if (*s1 == '\0' && *s2 == '*')
+			return wildcmp(s1, ++s2);
+		else if (*s1 == '*' && *s2 == '\0')
+			return wildcmp(++s1, s2);
+		return (0);
+	}
 
-	return (x * _pow_recursion(x, y - 1));
+	if (*s1 == *s2)
+	{
+		return wildcmp(++s1, ++s2);
+	}
+	else if (*s1 == '*')
+	{
+		if (*(s1 + 1) == '*')
+			return wildcmp(++s1, s2);
+		else
+		{
+			return wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2);
+		}
+	}
+	else if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return wildcmp(s1, ++s2);
+		else
+		{
+			return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
+		}
+	}
+
+	return (0);
+
+}
+
+int findsrc(char *s, char c, int i, int p)
+{
+	if (*(s + i) == '\0')
+		return (p + 1);
+	else if (*(s + i) == c || *(s + i) == '*')
+		p = i;
+
+	return (findsrc(s, c, i + 1, p));
 }

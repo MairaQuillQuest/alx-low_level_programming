d26186c4759929ef735a3964c3059324d865c432
MairaQuillQuest <ririsoniah@gmail.com>
2023-10-09 05:11:52 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -3,7 +3,7 @@
 
 /*
  * File: 0-object_like_macro.h
- * Auth: Moureen  Wangui 
+ * Auth: Moureen Wangui
  * Desc: Header file that defines a macro named
  *       SIZE as an abbreviation for the token 1024.
  */

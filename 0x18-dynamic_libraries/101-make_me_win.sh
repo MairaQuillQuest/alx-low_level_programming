d8bc9d97f148b74fafd868fcb7fb98f01dfc842b
MairaQuillQuest <ririsoniah@gmail.com>
2023-12-12 04:55:43 -0800

    fixed error

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo -e '#include <stdio.h>\n#include <stdlib.h>\nint rand(void) { static int called = 0; called++; if (called == 1) return 9; else exit(0); }' > /tmp/libhack.c
-gcc -shared -o /tmp/libhack.so /tmp/libhack.c
-LD_PRELOAD=/tmp/libhack.so ./gm 9 8 10 24 75 9
+wget -P /tmp/ https://raw.github.com/MairaQuillQuest/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so

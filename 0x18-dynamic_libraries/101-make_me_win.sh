d8ecef18a9c28034f6f7222ae0d397fc3103adbd
MairaQuillQuest <ririsoniah@gmail.com>
2023-12-11 16:23:34 -0800

    fixed the error

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo -e '#include <stdio.h>\n#include <stdlib.h>\nint rand(void) { return 9; }' > /tmp/libhack.c
+echo -e '#include <stdio.h>\n#include <stdlib.h>\nint rand(void) { static int called = 0; called++; if (called == 1) return 9; else exit(0); }' > /tmp/libhack.c
 gcc -shared -o /tmp/libhack.so /tmp/libhack.c
-LD_PRELOAD=/tmp/libhack.so
+LD_PRELOAD=/tmp/libhack.so ./gm 9 8 10 24 75 9
